7ea42586690e6f9f7fb61602f8361615ab7c1e73
yurzhang <yurzhang.oi@gmail.com>
2025-03-11 17:30:28 +0800

    feat: setup Umami

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -10,6 +10,11 @@ export default createHandler(() => (
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" type="image/png" href="/favicon.png" />
           <title>PastePlz</title>
+          <script
+            defer
+            src="https://cloud.umami.is/script.js"
+            data-website-id="9ca38a28-bb4a-409e-a545-fefb597c212c"
+          ></script>
           {assets}
         </head>
         <body>
